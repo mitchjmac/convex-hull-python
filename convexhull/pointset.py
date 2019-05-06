bd5c41cacbcfdeaa974d8895535244cc8a5f5301
Mitchell MacInnis <mitchjmac@gmail.com>
2019-05-05 19:26:40 -0500

    Update pointset comparison to exact match

--- convexhull/pointset.py
+++ convexhull/pointset.py
@@ -7,9 +7,9 @@ class PointSet:
     def __init__(self, pts=[]):
         self.points  = pts
 
-    def generate(self, size):
-        x = np.round_(2500*np.random.random((size,)),2)
-        y = np.round_(2500*np.random.random((size,)),2)
+    def generate(self, size, max_val=2500):
+        x = np.round_(max_val*np.random.random((size,)),2)
+        y = np.round_(max_val*np.random.random((size,)),2)
         no_dupes = OrderedDict()
         for p in zip(x, y):
             no_dupes.setdefault(p[:2], p)
@@ -19,14 +19,9 @@ class PointSet:
         with open(filepath, 'wb') as fp:
             pickle.dump(self.points, fp)
 
-    def restore(self, filepath):
+    def load(self, filepath):
         with open (filepath, 'rb') as fp:
             self.points = pickle.load(fp)
 
     def compare(self, ps):
-        me = set(self.points)
-        them = set(ps.points)
-        if (me - them) or (them - me):
-            return False
-        else:
-            return True
+        return self.points == ps.points
