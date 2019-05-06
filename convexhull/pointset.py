c673a315add388df0fff2cb91f0748b764c2ab1e
Mitchell MacInnis <mitchjmac@gmail.com>
2019-05-06 01:59:09 -0500

    Change pointset export to JSON

--- convexhull/pointset.py
+++ convexhull/pointset.py
@@ -1,27 +1,36 @@
 import pickle
 import numpy as np
 from collections import OrderedDict
+import simplejson
 
 class PointSet:
 
     def __init__(self, pts=[]):
         self.points  = pts
 
-    def generate(self, size, max_val=2500):
-        x = np.round_(max_val*np.random.random((size,)),2)
-        y = np.round_(max_val*np.random.random((size,)),2)
+    def generate(self, size):
+        x = np.round_(size*10*np.random.random((size,)),2)
+        y = np.round_(size*10*np.random.random((size,)),2)
         no_dupes = OrderedDict()
-        for p in zip(x, y):
-            no_dupes.setdefault(p[:2], p)
+        while True:
+            for p in zip(x, y):
+                no_dupes.setdefault(p[:2], p)
+            num = len(no_dupes)
+            if num == size:
+                break
+            else:
+                gen = size - num
+                x = np.round_(size*10*np.random.random((gen,)),2)
+                y = np.round_(size*10*np.random.random((gen,)),2)
         self.points = list(no_dupes.values())
 
     def save(self, filepath):
-        with open(filepath, 'wb') as fp:
-            pickle.dump(self.points, fp)
+        with open(filepath, 'w') as fp:
+            simplejson.dump(self.points, fp)
 
     def load(self, filepath):
-        with open (filepath, 'rb') as fp:
-            self.points = pickle.load(fp)
+        with open (filepath, 'r') as fp:
+            self.points = [tuple(l) for l in simplejson.load(fp)]
 
     def compare(self, ps):
         return self.points == ps.points
